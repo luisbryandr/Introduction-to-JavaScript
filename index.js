df6ed98c22c8f894e0a8058c6ee09481a98b4210
luisbryandr <luisbdominguezr@gmail.com>
2021-07-13 20:01:21 -0400

    solved all tasks bu 4

--- index.js
+++ index.js
@@ -228,11 +228,25 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
-  }
-  
-  
+function grade(score){
+  if(score >= 90 && score <= 100){
+    return 'you got an A'
+  }else if(score >= 80 && score <= 89){
+    return 'you got a B'
+  }else if(score >= 70 && score <= 79){
+    return 'you got a C'
+  }else if(score >= 60 && score <= 69){
+    return 'you got a D'
+}else {
+  return 'you got an F'
+}
+}
+
+console.log('Task 7', grade(87))
+console.log('Task 7', grade(97))
+console.log('Task 7', grade(66))
+console.log('Task 7', grade(72))
+console.log('Task 7', grade(52))
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
